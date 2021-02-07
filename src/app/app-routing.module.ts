dd3161e70641c167f8c422244a134bfc3965b9ba
Justin Magaram <justin@magaram.com>
2021-02-07 12:22:22 -0800

    router, experiment page, nothing on home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExperimentComponent } from './experiment/experiment.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'experiment', component: ExperimentComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/experiment/experiment.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiment',
+  templateUrl: './experiment.component.html',
+  styleUrls: ['./experiment.component.scss'],
+})
+export class ExperimentComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
