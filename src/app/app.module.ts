96669a77b339b277501c3ab25be0424ccd566cbb
Justin Magaram <justin@magaram.com>
2021-02-07 18:02:32 -0800

    add typed objects to firestore using simple form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { environment } from './../environments/environment';
@@ -17,6 +18,7 @@ import { ExperimentComponent } from './experiment/experiment.component';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/experiment/experiment.component.ts
+++ src/app/experiment/experiment.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { FormControl } from '@angular/forms';
+
+interface Animal {
+  type: string;
+  color: string;
+}
 
 @Component({
   selector: 'app-experiment',
@@ -12,15 +18,24 @@ export class ExperimentComponent implements OnInit {
   }
 
   fs: AngularFirestore;
+  animalType = new FormControl('');
+  animalColor = new FormControl('');
 
   ngOnInit(): void {}
 
-  onCreate(): void {
-    this.fs.firestore
-      .collection('animals')
-      .doc('dog')
-      .set({ type: 'Dog', color: 'Brown' })
+  onCreateAnimal(): void {
+    let collection = this.fs.collection<Animal>('animals');
+    let type = this.animalType.value as string;
+    let color = this.animalColor.value as string;
+    let animal: Animal = {
+      type: type,
+      color: color,
+    };
+    collection
+      .add(animal)
       .then(() => console.log('Wrote it!'))
-      .catch((error) => console.log('Error writing document: ', error));
+      .catch((error) => {
+        console.log('Error writing document: ', error);
+      });
   }
 }
