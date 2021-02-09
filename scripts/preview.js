00a035a37ad7c53c6424c057e77c1e184bffef07
Justin Magaram <justin@magaram.com>
2021-02-08 19:14:56 -0800

    better preview script - option for npm ci

--- scripts/preview.js
+++ scripts/preview.js
@@ -8,34 +8,55 @@ function exec(command) {
 }
 
 (async () => {
-  const shouldBuild = await prompts({
-    type: "confirm",
+  const build = await prompts({
+    type: "select",
     name: "value",
-    message: "Build first?",
-    initial: true,
+    message: "Build first",
+    choices: [
+      {
+        title: "Do not build",
+        description: "Use whatever is in the dist folder now",
+      },
+      {
+        title: "Quick build",
+        description: "Just npm run build",
+      },
+      {
+        title: "Full",
+        description: "Slower npm ci && npm run build",
+      },
+    ],
+    initial: 1,
   });
 
-  if (shouldBuild.value) {
-    exec("ng build");
+  switch (build.value) {
+    case 0:
+      break;
+    case 1:
+      exec("npm run build");
+      break;
+    case 2:
+      exec("npm ci && npm run build");
+      break;
   }
 
-  const shouldDeploy = await prompts({
+  const deploy = await prompts({
     type: "confirm",
     name: "value",
-    message: "Should deploy a preview?",
+    message: "Should deploy a preview",
     initial: true,
   });
 
-  if (!shouldDeploy.value) {
+  if (!deploy.value) {
     return;
   }
 
-  const channelName = await prompts({
+  const channel = await prompts({
     type: "text",
     name: "channel",
-    message: "Channel name?",
+    message: "Channel name",
     initial: "preview",
   });
 
-  exec("firebase hosting:channel:deploy " + channelName.channel);
+  exec("firebase hosting:channel:deploy " + channel.channel);
 })();
