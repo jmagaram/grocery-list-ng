ef384d3b2d5cb401a13bd8f142cb38b362e44c3a
Justin Magaram <justin@magaram.com>
2021-02-10 14:34:05 -0800

    use Inquirer rather than Prompts

--- scripts/preview.js
+++ scripts/preview.js
@@ -1,6 +1,6 @@
 // Deploys current build to a preview channel in Firebase
 
-const prompts = require("prompts");
+const inquirer = require("inquirer");
 const { execSync } = require("child_process");
 
 function exec(command) {
@@ -8,38 +8,40 @@ function exec(command) {
 }
 
 (async () => {
-  const build = await prompts({
-    type: "confirm",
-    name: "value",
-    message: "Start production build",
-    initial: true,
-  });
+  const build = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "value",
+      message: "Start production build",
+      default: true,
+    },
+  ]);
 
-  if (build) {
+  if (build.value) {
     exec("npm run-script prod");
   }
 
-  const deploy = await prompts({
+  const deploy = await inquirer.prompt({
     type: "confirm",
     name: "value",
     message: "Deploy a preview",
-    initial: true,
+    default: true,
   });
 
   if (!deploy.value) {
     return;
   }
 
-  const channel = await prompts({
-    type: "text",
-    name: "channel",
-    message: "Channel name",
-    initial: "preview",
-  });
+  const channel = await inquirer.prompt([
+    {
+      type: "input",
+      name: "value",
+      message: "Channel name",
+      initial: "preview",
+    },
+  ]);
 
-  exec("firebase hosting:channel:deploy " + channel.channel);
-
-  // This is really slow
+  exec("firebase hosting:channel:deploy " + channel.value);
   exec("rm -rf node_modules");
   exec("npm install");
 })();
