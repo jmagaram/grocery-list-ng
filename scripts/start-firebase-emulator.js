85690e6262bf83956dee2d7e7d4c6cbaeb2e6daa
Justin Magaram <justin@magaram.com>
2021-02-10 14:49:20 -0800

    prompt if to load save data; move to inquirer

--- scripts/start-firebase-emulator.js
+++ scripts/start-firebase-emulator.js
@@ -1,10 +1,26 @@
 // Starts the firebase emulator
-
-const prompts = require("prompts");
+const inquirer = require("inquirer");
 const { execSync } = require("child_process");
 
 function exec(command) {
   execSync(command, { stdio: "inherit" });
 }
 
-exec("firebase emulators:start");
+(async () => {
+  const save = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "value",
+      message: "Load and save data",
+      default: false,
+    },
+  ]);
+
+  if (save.value) {
+    const path =
+      "C:/Users/justi/source/repos/grocery-list-ng/firebase-emulator-data";
+    exec(`firebase emulators:start --import=${path} --export-on-exit`);
+  } else {
+    exec(`firebase emulators:start`);
+  }
+})();
