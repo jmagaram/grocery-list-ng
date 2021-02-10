34fe1d9dd3a95f6ba80a726efe8ad7ebe4407c55
Justin Magaram <justin@magaram.com>
2021-02-09 18:37:11 -0800

    remove firebaseuiconfig - not using it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,23 +22,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { AuthPlaygroundComponent } from './auth-playground/auth-playground.component';
 
-const firebaseUiConfig = {
-  enableFirestoreSync: true, // enable/disable autosync users with firestore
-  toastMessageOnAuthSuccess: false, // whether to open/show a snackbar message on auth success - default : true
-  toastMessageOnAuthError: false, // whether to open/show a snackbar message on auth error - default : true
-  authGuardFallbackURL: '/loggedout', // url for unauthenticated users - to use in combination with canActivate feature on a route
-  authGuardLoggedInURL: '/loggedin', // url for authenticated users - to use in combination with canActivate feature on a route
-  passwordMaxLength: 60, // `min/max` input parameters in components should be within this range.
-  passwordMinLength: 8, // Password length min/max in forms independently of each componenet min/max.
-  nameMaxLength: 50, // Same as password but for the name
-  nameMinLength: 2, // Same as password but for the name
-  // If set, sign-in/up form is not available until email has been verified.
-  // Plus protected routes are still protected even though user is connected.
-  guardProtectedRoutesUntilEmailIsVerified: true,
-  enableEmailVerification: true, // default: true
-  useRawUserCredential: true, // If set to true outputs the UserCredential object instead of firebase.User after login and signup - Default: false
-};
-
 // https://github.com/angular/angularfire/blob/master/docs/emulators/emulators.md
 // Do not understand why the port numbers are duplicated here and in the firebase.json file.
 const emulatorProviders: Provider[] = environment.useEmulators
