afd14f021cbd064a27e1009770160cc4e81a220e
Justin Magaram <justin@magaram.com>
2021-02-10 12:20:04 -0800

    incorrect comment delimiter in powershell

--- scripts/kill-emulator.ps1
+++ scripts/kill-emulator.ps1
@@ -1,7 +1,7 @@
-﻿// Kills the firebase emulator in cases where it did not shut down cleanly
-// Based on this information
-// https://dzhavat.github.io/2020/04/09/powershell-script-to-kill-a-process-on-windows.html
-// https://stackoverflow.com/questions/39632667/how-do-i-kill-the-process-currently-using-a-port-on-localhost-in-windows
+﻿# Kills the firebase emulator in cases where it did not shut down cleanly
+# Based on this information
+# https://dzhavat.github.io/2020/04/09/powershell-script-to-kill-a-process-on-windows.html
+# https://stackoverflow.com/questions/39632667/how-do-i-kill-the-process-currently-using-a-port-on-localhost-in-windows
 function killProcessByPort($port) {
     $foundProcesses = netstat -ano | findstr :$port
     $activePortPattern = ":$port\s.+LISTENING\s+\d+$"
