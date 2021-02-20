0fe65a6c3e8573acb925d1a5561f8e814cb9f95d
Justin Magaram <justin@magaram.com>
2021-02-20 12:54:46 -0800

    smarter uppercasing function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,34 +3,35 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
+type Message = { asTypedByUser: string; uppercased: string | null };
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', { structuredData: true });
   response.send('Hello from Firebase!');
 });
 
 export const addMessage = functions.https.onRequest(async (req, res) => {
-  const original = req.query.text;
+  let message: Message = {
+    asTypedByUser: req.query.text as string,
+    uppercased: null,
+  };
   const writeResult = await admin
     .firestore()
     .collection('messages')
-    .add({ original: original });
+    .add(message);
   res.json({ result: `Message with ID: ${writeResult.id} added.` });
 });
 
 export const makeUppercase = functions.firestore
   .document('/messages/{documentId}')
-  .onCreate((snap, context) => {
-    const original = snap.data().original;
-    functions.logger.log('Uppercasing', context.params.documentId, original);
-    const uppercase = original.toUpperCase();
-    return snap.ref.set({ uppercase }, { merge: true });
-  });
-
-export const makeUppercaseOnUpdate = functions.firestore
-  .document('/messages/{documentId}')
-  .onUpdate((snap, context) => {
-    const original = snap.after.data().original;
-    functions.logger.log('Uppercasing', context.params.documentId, original);
-    const uppercase = original.toUpperCase();
-    return snap.after.ref.set({ uppercase }, { merge: true });
+  .onWrite((snap, context) => {
+    const before = snap.before.data() as Message | undefined;
+    const after = snap.after.data() as Message;
+    if (before == undefined || before.asTypedByUser != after.asTypedByUser) {
+      after.uppercased = after.asTypedByUser.toUpperCase();
+      functions.logger.log('UPPERCASING', context.params.documentId, after);
+      return snap.after.ref.set(after);
+    } else {
+      return Promise.resolve();
+    }
   });
